755d67b1ed3cfa9aba3bd48a82c5f697530c277d
MusicYYin <yinyue20020723@gmail.com>
2026-09-26 16:50:48 +0800

    Fix IPlayerCharacter namespace in radar

--- Soumen/Services/FrontlineRadarService.cs
+++ Soumen/Services/FrontlineRadarService.cs
@@ -1,6 +1,6 @@
 using System.Numerics;
 using Dalamud.Bindings.ImGui;
-using Dalamud.Game.ClientState.Objects.Types;
+using Dalamud.Game.ClientState.Objects.SubKinds;
 using Dalamud.Interface.Utility;
 using NativeBattleChara = FFXIVClientStructs.FFXIV.Client.Game.Character.BattleChara;
 
